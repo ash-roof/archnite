6595abeaefae82d93fd8e008997def8dcec742af
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-01-27 20:10:42 +0200

    made trigram index case-insensitive

--- archnite-db-populator/schema.sql
+++ archnite-db-populator/schema.sql
@@ -1,11 +1,12 @@
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
 CREATE TABLE IF NOT EXISTS arch_packages (
     id SERIAL PRIMARY KEY,
     architecture VARCHAR(10),
-    package_name VARCHAR(100),
-    description TEXT,
-    last_update timestamptz,
+    package_name TEXT NOT NULL,
+    description TEXT NOT NULL,
+    last_update TIMESTAMPTZ NOT NULL,
     url TEXT
 );
 
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE INDEX IF NOT EXISTS archpkg_idx_trgm ON arch_packages USING gin((package_name) gin_trgm_ops);
+CREATE INDEX IF NOT EXISTS archpkg_idx_trgm ON arch_packages USING gin (lower(package_name) gin_trgm_ops);
