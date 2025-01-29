48fcf3c73714c5b3d7de7ab4903da84169dff1ec
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-01-29 14:55:43 +0200

    added repository for ArchPackage with search query, temporarily removed docker compose from pom.xml

---
+++ archnite-spring/src/main/java/dev/omarashraf/archnite/controller/ArchPackageController.java
@@ -0,0 +1,4 @@
+package dev.omarashraf.archnite.controller;
+
+public class ArchPackageController {
+}
---
+++ archnite-spring/src/main/java/dev/omarashraf/archnite/repository/ArchPackageRepository.java
@@ -0,0 +1,19 @@
+package dev.omarashraf.archnite.repository;
+
+import dev.omarashraf.archnite.model.ArchPackage;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface ArchPackageRepository extends CrudRepository<ArchPackage, Integer> {
+    @Query(value = """
+        SELECT *
+        FROM arch_packages
+        WHERE package_name LIKE CONCAT('%', :keyword, '%')
+        ORDER BY similarity(package_name, :keyword) DESC
+        LIMIT :numResults
+    """, nativeQuery = true)
+    List<ArchPackage> searchArchPackagesBySimilarity(@Param("keyword") String keyword, @Param("numResults") int numResults);
+}
