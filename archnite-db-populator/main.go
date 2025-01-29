9694f02576fc68a17b0e057d99ce698fdbdde339
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-01-29 02:13:09 +0200

    changed run interval to 6 hours and added initial run on startup

--- archnite-db-populator/main.go
+++ archnite-db-populator/main.go
@@ -8,9 +8,13 @@ import (
 )
 
 func main() {
-	ticker := time.NewTicker(time.Minute * 3)
-	defer ticker.Stop()
+	if err := arch.Populate(); err != nil {
+		fmt.Printf("application exited with error: %v\n", err)
+		os.Exit(1)
+	}
 
+	ticker := time.NewTicker(time.Hour * 6)
+	defer ticker.Stop()
 	for {
 		select {
 		case <-ticker.C:
