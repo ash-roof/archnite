b51547d83702047616fe0717a6a3da98a6a71f71
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-01-30 15:30:12 +0200

    added extra unit tests for arch, created initial files for AUR

--- archnite-db-populator/internal/arch/arch_test.go
+++ archnite-db-populator/internal/arch/arch_test.go
@@ -4,6 +4,13 @@ import (
 	"testing"
 )
 
+func TestLoadPageResponse(t *testing.T) {
+	res, err := loadPageResponse(1)
+	if len(res.Results) == 0 || err != nil {
+		t.Fatalf(`loadPageResponse(1) failed: ResultsLen=%d, err=%v, want ResultsLen=250, err=nil`, len(res.Results), err)
+	}
+}
+
 func TestLoadPageResponseLowerBound(t *testing.T) {
 	res, err := loadPageResponse(-1)
 	if len(res.Results) != 0 || err == nil {
@@ -18,6 +25,13 @@ func TestLoadPageResponseUpperBound(t *testing.T) {
 	}
 }
 
+func TestLoadPackages(t *testing.T) {
+	res, err := loadPackages()
+	if len(res) < 14000 || err != nil {
+		t.Fatalf(`loadPackages() failed: ResultsLen=%d, err=%v, want ResultsLen>14000, err=nil`, len(res), err)
+	}
+}
+
 func BenchmarkLoadPackages(b *testing.B) {
-	LoadPackages()
+	loadPackages()
 }
