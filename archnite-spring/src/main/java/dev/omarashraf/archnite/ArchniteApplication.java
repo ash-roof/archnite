1e9ff090d4908540d13c082d81008c160316dd02
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-01-31 16:53:56 +0200

    added .env support to spring project and removed hardcoded variables

--- archnite-spring/src/main/java/dev/omarashraf/archnite/ArchniteApplication.java
+++ archnite-spring/src/main/java/dev/omarashraf/archnite/ArchniteApplication.java
@@ -1,5 +1,6 @@
 package dev.omarashraf.archnite;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ArchniteApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().directory("../").load();
+
+        setEnvVar(dotenv, "SPRING_DATASOURCE_URL");
+        setEnvVar(dotenv, "SPRING_DATASOURCE_USERNAME");
+        setEnvVar(dotenv, "SPRING_DATASOURCE_PASSWORD");
         SpringApplication.run(ArchniteApplication.class, args);
     }
 
+    private static void setEnvVar(Dotenv dotenv, String key) {
+        String value = dotenv.get(key);
+        if (value != null) {
+            System.setProperty(key, value);
+        }
+    }
 }
