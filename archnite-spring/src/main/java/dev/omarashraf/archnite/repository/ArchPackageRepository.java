eda8b1798e16e4347c4556d36b6978707338794f
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-02-07 20:13:09 +0200

    improved search query by adding multiple sorting criteria

--- archnite-spring/src/main/java/dev/omarashraf/archnite/repository/ArchPackageRepository.java
+++ archnite-spring/src/main/java/dev/omarashraf/archnite/repository/ArchPackageRepository.java
@@ -14,9 +14,13 @@ public interface ArchPackageRepository extends JpaRepository<ArchPackage, Intege
     @Query(value = """
         SELECT *
         FROM arch_packages
-        WHERE package_name ILIKE CONCAT('%', :keyword, '%')
-        ORDER BY similarity(package_name, :keyword) DESC
-        LIMIT :limit
+        WHERE package_name ILIKE '%' || :keyword || '%'
+        ORDER BY
+            CASE WHEN package_name ILIKE :keyword THEN 0 ELSE 1 END,
+            CASE WHEN package_name ILIKE :keyword || '%' THEN 0 ELSE 1 END,
+            similarity(package_name, :keyword) DESC,
+            length(package_name)
+        LIMIT :limit;
     """, nativeQuery = true)
     List<ArchPackage> searchArchPackagesBySimilarity(@Param("keyword") String keyword, @Param("limit") int limit);
 
