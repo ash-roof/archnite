a6b9a73c94898f9a6188d248153b894172d6e498
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-02-14 18:17:12 +0200

    created default error response class

--- archnite-spring/src/main/java/dev/omarashraf/archnite/exception/GlobalExceptionHandler.java
+++ archnite-spring/src/main/java/dev/omarashraf/archnite/exception/GlobalExceptionHandler.java
@@ -1,23 +1,22 @@
 package dev.omarashraf.archnite.exception;
 
+import dev.omarashraf.archnite.model.ErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, String>> handleGenericException(Exception ex) {
-        Map<String, String> response = new HashMap<>();
-        response.put("error", ex.getMessage());
+    public ResponseEntity<?> handleGenericException(Exception ex) {
+        ErrorResponse response = new ErrorResponse(LocalDateTime.now(), ex.getMessage(), "Internal Server Error");
 
         System.out.println("Exception thrown: " + ex.getClass().getName());
         ex.printStackTrace();
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
+        return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
---
+++ archnite-spring/src/main/java/dev/omarashraf/archnite/model/ErrorResponse.java
@@ -0,0 +1,16 @@
+package dev.omarashraf.archnite.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private LocalDateTime timestamp;
+    private String message;
+    private String details;
+}
