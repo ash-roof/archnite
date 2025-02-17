c2ab3b4006e4bbcd6b23687aa4affd91f8bed5cb
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-02-17 22:48:25 +0200

    created enum converter to allow case-insensitive enums in query params

--- archnite-spring/src/main/java/dev/omarashraf/archnite/util/CaseInsensitiveEnumConverter.java
+++ archnite-spring/src/main/java/dev/omarashraf/archnite/util/CaseInsensitiveEnumConverter.java
@@ -1,4 +1,16 @@
 package dev.omarashraf.archnite.util;
 
-public class CaseInsensitiveEnumConverter {
+import org.springframework.core.convert.converter.Converter;
+
+public class CaseInsensitiveEnumConverter<T extends Enum<T>> implements Converter<String, T> {
+    private final Class<T> enumClass;
+
+    public CaseInsensitiveEnumConverter(Class<T> enumClass) {
+        this.enumClass = enumClass;
+    }
+
+    @Override
+    public T convert(String from) {
+        return T.valueOf(enumClass, from.toUpperCase());
+    }
 }
