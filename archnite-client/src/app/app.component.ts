b122867868087bca357da011f17c1beb8f8197a5
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-02-26 16:19:13 +0200

    changed page title

--- archnite-client/src/app/app.component.ts
+++ archnite-client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'archnite-client';
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Archnite');
+  }
 }
