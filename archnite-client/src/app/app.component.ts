904469a2c08bce1f1fe55369cc3c29817a6d28d0
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-03-14 02:24:33 +0200

    added test button

--- archnite-client/src/app/app.component.ts
+++ archnite-client/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Title } from '@angular/platform-browser';
+import { ButtonComponent } from './button/button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
