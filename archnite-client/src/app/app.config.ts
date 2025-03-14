6203abba376280fbad5b949fde43922d92befef0
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-03-14 02:24:41 +0200

    added primeng tailwind integration

--- archnite-client/src/app/app.config.ts
+++ archnite-client/src/app/app.config.ts
@@ -12,7 +12,13 @@ export const appConfig: ApplicationConfig = {
   provideAnimationsAsync(),
   providePrimeNG({
     theme: {
-      preset: Aura
+      preset: Aura,
+      options: {
+        cssLayer: {
+          name: 'primeng',
+          order: 'theme, base, primeng'
+        }
+      }
     },
     ripple: true
   })]
