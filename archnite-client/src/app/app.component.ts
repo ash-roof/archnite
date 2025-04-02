d6ee0123f9bac22459e4bed93d7e52f9f928ec2d
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-04-02 14:08:28 +0200

    finished basic search functionality

--- archnite-client/src/app/app.component.ts
+++ archnite-client/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { Title } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { AutoCompleteModule } from 'primeng/autocomplete';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ArchPackage } from '../model/archpackage.model';
+import { ArchPackageService } from '../service/archpackage.service';
 
 interface AutoCompleteCompleteEvent {
   originalEvent: Event;
@@ -11,19 +14,29 @@ interface AutoCompleteCompleteEvent {
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonModule, AutoCompleteModule, FormsModule],
+  imports: [ButtonModule, AutoCompleteModule, FormsModule, RadioButtonModule],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
-  selectedPackages: string[];
+  selectedPackages: ArchPackage[];
   suggestedPackages: string[];
+  packageType: string;
 
-  constructor(private titleService: Title) {
-    this.titleService.setTitle("Archnite")
-    this.suggestedPackages = []
-    this.selectedPackages = []
+  constructor(private titleService: Title, private archPackageService: ArchPackageService) {
+    this.titleService.setTitle("Archnite");
+    this.suggestedPackages = [];
+    this.selectedPackages = [];
+    this.packageType = "All";
   }
   search(event: AutoCompleteCompleteEvent) {
-    this.suggestedPackages = [...Array(10).keys()].map((item) => event.query + '-' + item);
+    this.archPackageService.searchPackages(event.query, this.packageType).subscribe({
+      next: (results: ArchPackage[]) => {
+        this.suggestedPackages = results.map((archPackage) => archPackage.packageName);
+      },
+      error: (err) => {
+        console.error('Package search failed', err);
+        this.suggestedPackages = [];
+      }
+    });
   }
 }
--- archnite-client/src/app/app.config.ts
+++ archnite-client/src/app/app.config.ts
@@ -2,9 +2,13 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import ArchnitePreset from './archnitepreset';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideHttpClient(
+      withFetch(),
+    ),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimationsAsync(),
     providePrimeNG({
