e5844a9ae80fafe76bd18a29bfbfc459b5b8c4e9
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-04-02 14:57:36 +0200

    copy to clipboard

--- archnite-client/src/app/app.component.ts
+++ archnite-client/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { AutoCompleteModule } from 'primeng/autocomplete';
 import { RadioButtonModule } from 'primeng/radiobutton';
+import { ButtonModule } from 'primeng/button';
+
 import { ArchPackage } from '../model/archpackage.model';
 import { ArchPackageService } from '../service/archpackage.service';
 
@@ -19,7 +20,7 @@ interface AutoCompleteCompleteEvent {
 })
 export class AppComponent {
   selectedPackages: ArchPackage[];
-  suggestedPackages: string[];
+  suggestedPackages: ArchPackage[];
   packageType: string;
 
   constructor(private titleService: Title, private archPackageService: ArchPackageService) {
@@ -28,15 +29,35 @@ export class AppComponent {
     this.selectedPackages = [];
     this.packageType = "All";
   }
+
   search(event: AutoCompleteCompleteEvent) {
     this.archPackageService.searchPackages(event.query, this.packageType).subscribe({
       next: (results: ArchPackage[]) => {
-        this.suggestedPackages = results.map((archPackage) => archPackage.packageName);
+        this.suggestedPackages = results;
       },
       error: (err) => {
         console.error('Package search failed', err);
         this.suggestedPackages = [];
       }
     });
+  }
+
+  handleCopyClick(pkg: ArchPackage, event: Event) {
+    event.preventDefault();
+    event.stopPropagation();
+    window.open(pkg.url, '_blank');
+  }
+
+  copyToClipboard() {
+    if (this.selectedPackages.length === 0) return;
+
+    const packageNames = this.selectedPackages.map(pkg => pkg.packageName).join(' ');
+    navigator.clipboard.writeText(packageNames)
+      .then(() => {
+        console.log('Copied to clipboard:', packageNames);
+      })
+      .catch(err => {
+        console.error('Failed to copy:', err);
+      });
   }
 }
