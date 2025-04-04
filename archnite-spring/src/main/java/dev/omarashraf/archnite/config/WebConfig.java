c843c8cd5b8c8ef81c40a1d64b1caae095bb6095
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-04-04 23:22:37 +0200

    only allow get methods

--- archnite-spring/src/main/java/dev/omarashraf/archnite/config/WebConfig.java
+++ archnite-spring/src/main/java/dev/omarashraf/archnite/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("*")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedMethods("GET")
                         .allowedHeaders("*")
                         .allowCredentials(false);
             }
