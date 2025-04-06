eeaf5b95a8b43bb808688dd025f46abeb0d334f8
OmarAshraf-02 <omar.ashraf.58.02@gmail.com>
2025-04-06 23:15:08 +0200

    added rate limiting

--- archnite-spring/src/main/java/dev/omarashraf/archnite/ArchniteApplication.java
+++ archnite-spring/src/main/java/dev/omarashraf/archnite/ArchniteApplication.java
@@ -3,8 +3,10 @@ package dev.omarashraf.archnite;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class ArchniteApplication {
 
     public static void main(String[] args) {
